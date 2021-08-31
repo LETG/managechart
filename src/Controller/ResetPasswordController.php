3e76b9c8cff88d75651eb1a6924f1bf671adebef
kimlai <hello@kimlaitrinh.me>
2021-08-31 10:36:54 +0200

    fix redirection after login and password reset

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -123,7 +123,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('index');
         }
 
         return $this->render('reset_password/reset.html.twig', [
